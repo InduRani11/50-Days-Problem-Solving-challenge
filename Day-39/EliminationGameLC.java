1fc3b6b3a40aee3f211c8ea97d6d1276afc82d6d
Indu Rani <168950679+InduRani11@users.noreply.github.com>
2024-10-11 22:22:48 +0530

    Day-39

---
+++ Day-39/EliminationGameLC.java
@@ -0,0 +1,12 @@
+public class EliminationGameLC {
+    public static int lastRemaining(int n) {
+        if(n==1){
+            return 1;
+        }
+        return 2*(1 + n / 2 - lastRemaining(n/2));
+    }
+    public static void main(String[] args) {
+        int n=9;
+        System.out.println(lastRemaining(n));
+    }
+}
